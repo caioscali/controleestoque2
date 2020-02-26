a9123597cbabe7c6fa85b4e61c3f789d14304716
caioscali <caioscali@gmail.com>
2020-02-26 10:51:23 -0300

    projeto 2

--- produto.php
+++ produto.php
@@ -145,145 +145,146 @@ endif;
                             '<?php echo $value->numeroPatrimonio; ?>',
                             '<?php echo $value->idCategoria; ?>',
                             '<?php echo $value->id; ?>',')">Alterar</button>
-                         <form class="form_excluir" method="post" style="float: left; margin: 0 15px">
-                            <input type="hidden" name="id" value="<?php echo $value->id; ?>">
-                            <button name="excluir" type="submit"  class="btn btn-danger">Excluir</button>
-                        </form>
-                    </td>
-                </tr>
-            </tbody>
-            <?php
-        endforeach; ?>
-    </table>
-</div>
-<!-- Modal Cadastro Produto -->
-<div class="modal fade modal_cadastrar_produto" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Cadastro Produto</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form class="col-auto " method="post">
-                    <div class="container">
-                        <div class="row col-12">
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <label>Categoria:</label>
-                                    <select class="form-control" name="categoriaSelecionada" required="required">
-                                        <?php
-                                        foreach ($categoria->findAll() as $key => $val) : ?>
-                                            <option value="<?php echo ($val->id); ?>"> <?php echo $val->descricao; ?> </option>
+                            <form class="form_excluir" method="post" style="float: left; margin: 0 15px">
+                                <input type="hidden" name="id" value="<?php echo $value->id; ?>">
+                                <button name="excluir" type="submit"  class="btn btn-danger">Excluir</button>
+                            </form>
+                        </td>
+                    </tr>
+                </tbody>
+                <?php
+            endforeach; ?>
+        </table>
+    </div>
+    <!-- Modal Cadastro Produto -->
+    <div class="modal fade modal_cadastrar_produto" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Cadastro Produto</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form class="col-auto " method="post">
+                        <div class="container">
+                            <div class="row col-12">
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label>Categoria:</label>
+                                        <select class="form-control" name="categoriaSelecionada" required="required">
                                             <?php
-                                        endforeach; ?>
-                                    </select>
+                                            foreach ($categoria->findAll() as $key => $val) : ?>
+                                                <option value="<?php echo ($val->id); ?>"> <?php echo $val->descricao; ?> </option>
+                                                <?php
+                                            endforeach; ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label></label>
+                                        <a type="submit" class="btn btn-primary" href="categorias">Cadastrar Categoria</a>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <label></label>
-                                    <a type="submit" class="btn btn-primary" href="categorias">Cadastrar Categoria</a>
+                            <div class="row col-12">
+                                <div class="col-4">
+                                    <div class="form-group">
+                                        <label>Descricao:</label>
+                                        <input type="text" class="form-control" name="descricao" required="required">
+                                    </div>
                                 </div>
+                                <div class="col-4">
+                                    <div class="form-group">
+                                        <label>Marca:</label>
+                                        <input type="text" class="form-control" name="marca" required="required">
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="form-group">
+                                        <label>Número de Patrimonio:</label>
+                                        <input type="text" class="form-control" name="numeroPatrimonio" required="required">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row col-12">
+                                <div class="col-5"></div>
+                                <div class="col-4">
+                                    <button type="submit" name="cadastrar" class="btn btn-primary">Cadastrar</button>
+                                </div>
+                                <div class="col-4"></div>
                             </div>
                         </div>
-                        <div class="row col-12">
-                            <div class="col-4">
-                                <div class="form-group">
-                                    <label>Descricao:</label>
-                                    <input type="text" class="form-control" name="descricao" required="required">
-                                </div>
-                            </div>
-                            <div class="col-4">
-                                <div class="form-group">
-                                    <label>Marca:</label>
-                                    <input type="text" class="form-control" name="marca" required="required">
-                                </div>
-                            </div>
-                            <div class="col-4">
-                                <div class="form-group">
-                                    <label>Número de Patrimonio:</label>
-                                    <input type="text" class="form-control" name="numeroPatrimonio" required="required">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row col-12">
-                            <div class="col-5"></div>
-                            <div class="col-4">
-                                <button type="submit" name="cadastrar" class="btn btn-primary">Cadastrar</button>
-                            </div>
-                            <div class="col-4"></div>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<!-- Modal Alterar Produto -->
-<div class="modal fade modal_alterar_produto" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Alterar Produto</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form class="col-auto " method="post">
-                    <div class="container">
-                        <div class="row col-12">
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <label>Categoria:</label>
-                                    <select class="form-control" id="text_categoriaSelecionada" name="text_categoriaSelecionada" required="required">
-                                        <option>Selecione a Categoria</option>
-                                        <?php
-                                        foreach ($categoria->findAll() as $key => $val) : ?>
-                                            <option value="<?php echo ($val->id); ?>"> <?php echo $val->descricao; ?> </option>
+    <!-- Modal Alterar Produto -->
+    <div class="modal fade modal_alterar_produto" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Alterar Produto</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form class="col-auto " method="post">
+                        <div class="container">
+                            <div class="row col-12">
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label>Categoria:</label>
+                                        <select class="form-control" id="text_categoriaSelecionada" name="text_categoriaSelecionada" required="required">
+                                            <option>Selecione a Categoria</option>
                                             <?php
-                                        endforeach; ?>
-                                    </select>
+                                            foreach ($categoria->findAll() as $key => $val) : ?>
+                                                <option value="<?php echo ($val->id); ?>"> <?php echo $val->descricao; ?> </option>
+                                                <?php
+                                            endforeach; ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label></label>
+                                        <a type="submit" class="btn btn-primary" href="categorias">Cadastrar Categoria</a>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <label></label>
-                                    <a type="submit" class="btn btn-primary" href="categorias">Cadastrar Categoria</a>
+                            <div class="row col-12">
+                                <div class="col-4">
+                                    <div class="form-group">
+                                        <label>Descricao:</label>
+                                        <input type="text" class="form-control" id="text_descricao" name="text_descricao" required="required">
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="form-group">
+                                        <label>Marca:</label>
+                                        <input type="text" class="form-control" id="text_marca" name="text_marca" required="required">
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="form-group">
+                                        <label>Número de Patrimonio:</label>
+                                        <input type="text" class="form-control" id="text_numeroPatrimonio" name="text_numeroPatrimonio" required="required">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row col-12">
-                            <div class="col-4">
-                                <div class="form-group">
-                                    <label>Descricao:</label>
-                                    <input type="text" class="form-control" id="text_descricao" name="text_descricao" required="required">
+                            <div class="row col-12">
+                                <div class="col-5"></div>
+                                <div class="col-4">
+                                    <input type="hidden" name="text_id" id="text_id">
+                                    <input type="hidden" name="text_idDepartamentoProduto" id="text_idDepartamentoProduto">
+                                    <input type="submit" name="alterar" class="btn btn-primary" value="Alterar">
                                 </div>
+                                <div class="col-4"></div>
                             </div>
-                            <div class="col-4">
-                                <div class="form-group">
-                                    <label>Marca:</label>
-                                    <input type="text" class="form-control" id="text_marca" name="text_marca" required="required">
-                                </div>
-                            </div>
-                            <div class="col-4">
-                                <div class="form-group">
-                                    <label>Número de Patrimonio:</label>
-                                    <input type="text" class="form-control" id="text_numeroPatrimonio" name="text_numeroPatrimonio" required="required">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row col-12">
-                            <div class="col-5"></div>
-                            <div class="col-4">
-                                <input type="hidden" name="text_id" id="text_id">
-                                <input type="hidden" name="text_idDepartamentoProduto" id="text_idDepartamentoProduto">
-                                <input type="submit" name="alterar" class="btn btn-primary" value="Alterar">
-                            </div>
-                            <div class="col-4"></div>
                         </div>
                     </form>
                 </div>
