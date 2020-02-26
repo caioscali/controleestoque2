a21ed8a7c1aa0804ac3f69ef0d21b60deead9b9f
caioscali <caioscali@gmail.com>
2020-02-26 11:28:28 -0300

    projeto 2

--- produto.php
+++ produto.php
@@ -184,7 +184,7 @@ endif;
                                 </div>
                                 <div class="col-6">
                                     <div class="form-group">
-                                        <br /><br />
+                                        <br />
                                         <a type="submit" class="btn btn-primary" href="categorias">Cadastrar Categoria</a>
                                     </div>
                                 </div>
