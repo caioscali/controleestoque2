de4c0df1e387189252d460caf96b7cdf91d50fb6
caioscali <caioscali@gmail.com>
2020-02-26 10:46:32 -0300

    projeto 2

--- js/produto.js
+++ js/produto.js
@@ -1,13 +1,10 @@
-function load_modal(descricao, marca, numeroPatrimonio, idCategoria, quantidade, dataCadastro, idDepartamento, id, idDepartamentoProduto) {
+function load_modal(descricao, marca, numeroPatrimonio, idCategoria, id) {
   debugger;
   $('#text_id').val(id);
-  $('#text_idDepartamentoProduto').val(idDepartamentoProduto);
   $('#text_descricao').val(descricao);
   $('#text_marca').val(marca);
   $('#text_numeroPatrimonio').val(numeroPatrimonio);
   $('#text_categoriaSelecionada').val(idCategoria);
-  $('#text_quantidade').val(quantidade);
-  $('#text_dataCadastro').val(dataCadastro);
-  $('#text_departamentoSelecionado').val(idDepartamento);
+ 
 }
 
